775276e4a74a0fd8adfe6020ce2c49c5b1561341
CacaAlves <cacazinho99@outlook.com>
2020-05-24 10:14:55 -0300

    Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department d = new Department(1, "Books");
-		System.out.println(d);
+		Seller s = new Seller(1, "Cacá", "caca@o.com", new Date(), 3000.00, d);
+		System.out.println(s);
 	}
 
 }
