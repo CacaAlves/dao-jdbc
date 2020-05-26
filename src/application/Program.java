85593398262baf312c7f2dc021de8c6f5e314c85
CacaAlves <cacazinho99@outlook.com>
2020-05-26 14:47:54 -0300

    FinById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,17 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department d = new Department(1, "Books");
-		Seller s = new Seller(1, "Cacá", "caca@o.com", new Date(), 3000.00, d);
-		System.out.println(s);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(2);
+		
+		System.out.println(seller);
 	}
 
 }
